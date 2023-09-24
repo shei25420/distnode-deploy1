c3f9b9ef1c3d69a342fa5e5c2a7fcdb952fdda7b
shei25420 <chegeeric66@gmail.com>
2023-09-24 04:13:04 +0300

    Adding a test suite and code coverage

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const server = require('fastify')();
 const HOST = process.env.HOST || "127.0.0.1";
+/* istanbul ignore next */
 const PORT = process.env.PORT || 8000;
-const Recipe = require('recipe.js');
+const Recipe = require('./recipe.js');
 
 server.get('/', async (req, reply) => {
     return "Hello from distributed node";
@@ -14,9 +15,12 @@ server.get('/recipes/:id', async (req, reply) => {
 });
 
 (async () => {
-    await server.listen({
+    server.listen({
         host: HOST,
         port: PORT
+    }, (err, address) => {
+        /* istanbul ignore next */
+        if (err) throw err;
+        console.log(`server running at ${address}\n`);
     });
-    console.log(`Server running at ${HOST}:${PORT}\n`);
 })();
---
+++ test/intergration.js
@@ -0,0 +1,33 @@
+const { spawn } = require('child_process');
+const test = require('tape');
+
+const serverStart = () => new Promise ((resolve, reject) => {
+   const server = spawn('node', ['../server.js'], {
+       env: Object.assign({}, process.env, { PORT: 0 }),
+       cwd: __dirname
+   });
+
+   server.stdout.once('data', async (data) => {
+       const message = data.toString().trim();
+       const url = /server running at (.*)/.exec(message)[1];
+       resolve({ server, url });
+   });
+});
+
+test('GET /recipes/42', async (t) => {
+   const { server, url } = await serverStart();
+   const results = await fetch(`${url}/recipes/42`);
+
+   const body = await results.json();
+   t.equal(body.id, 42);
+   server.kill();
+});
+
+test('GET /', async (t) => {
+    const { server, url } = await serverStart();
+    const results = await fetch(`${url}/`);
+    const body = await results.text();
+
+    t.equal(body, "Hello from distributed node");
+    server.kill();
+});
